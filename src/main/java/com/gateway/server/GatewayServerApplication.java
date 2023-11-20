9995d5689720ef375df3c404a752848c929ea91d
Maneesh Murali <maneeshmurali@Maneeshs-MacBook-Air.local>
2023-11-20 12:40:10 +0530

    Gateway Server

---
+++ src/main/java/com/gateway/server/GatewayServerApplication.java
@@ -0,0 +1,19 @@
+package com.gateway.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder()
+		.profiles("dev")
+		.sources(GatewayServerApplication.class)
+		.run(args);
+	}
+
+}
