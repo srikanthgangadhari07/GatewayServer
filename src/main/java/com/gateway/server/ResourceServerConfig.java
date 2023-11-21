99bd621f656cc7d10edd23e47d9477304b4054fc
Maneesh Murali <maneeshmurali@Maneeshs-MacBook-Air.local>
2023-11-21 15:09:20 +0530

    resource server changes

---
+++ src/main/java/com/gateway/server/ResourceServerConfig.java
@@ -0,0 +1,35 @@
+package com.gateway.server;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.bouncycastle.jcajce.spec.ScryptKeySpec;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+@SuppressWarnings("removal")
+public class ResourceServerConfig {
+	
+	
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+		httpSecurity.authorizeExchange().anyExchange().authenticated()
+		.and()
+		.oauth2ResourceServer().jwt();
+		return httpSecurity.build();
+	}
+	
+	@Bean
+	public ReactiveJwtDecoder passwordEncoder() {
+		byte[] bytes="123456789012345678901234567890AB".getBytes();
+		SecretKeySpec secretKeySpec=new SecretKeySpec(bytes, 0, bytes.length,"AES");
+		return NimbusReactiveJwtDecoder.withSecretKey(secretKeySpec).build();
+	}
+
+}
