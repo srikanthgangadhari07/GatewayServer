03129b380540ca4970977f386301169819994559
tutamahesh <85124095+tutamahesh@users.noreply.github.com>
2024-08-28 11:16:29 +0530

    GatewayServer

--- src/main/java/com/gateway/server/GatewayServerApplication.java
+++ src/main/java/com/gateway/server/GatewayServerApplication.java
@@ -2,11 +2,13 @@ package com.gateway.server;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
